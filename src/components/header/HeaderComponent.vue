30aef331e89d9c5b856c1d40f355fd1dc8880fc3
alejandro.cano@imagineapps.co <alejandro.cano@imagineapps.co>
2024-04-15 19:58:52 -0500

    added menu

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
+import './header-component.scss'
 import { ref } from 'vue'
 
 import { setDarkMode } from '@/utils/utils'
+import DesktopMenu from './menu/DesktopMenu.vue';
 
 const isDarkMode = ref<boolean>(localStorage.getItem('isDarkMode') === 'true')
 setDarkMode(isDarkMode.value)
@@ -11,13 +13,20 @@ const handleTheme = () => {
   setDarkMode(isDarkMode.value)
 }
 
-
 </script>
 
 <template>
-  <nav class="outer">
 
-  </nav>
+  <div class="desktop">
+    <DesktopMenu />
+  </div>
+
+  <div class="mobile">
+    mobile
+  </div>
+
 </template>
 
+<style lang="scss" scoped>
 
+</style>
---
+++ src/components/header/menu/DesktopMenu.vue
@@ -0,0 +1,75 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import { setDarkMode } from '@/utils/utils'
+
+const isDarkMode = ref<boolean>(localStorage.getItem('isDarkMode') === 'true')
+setDarkMode(isDarkMode.value)
+
+const handleTheme = () => {
+  isDarkMode.value = !isDarkMode.value
+  setDarkMode(isDarkMode.value)
+}
+
+
+</script>
+
+<template>
+  <nav class="outer">
+
+    <div class="left">
+      <router-link to="/" class="link">
+        <p class="capital">
+          Estrella <span class="invest">Immobilien GmbH</span>
+        </p>
+      </router-link>
+      <div>
+        hello
+      </div>
+    </div>
+
+    <div class="right">
+      <div>hello</div>
+      <div>good bye</div>
+    </div>
+      
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.outer {
+  outline: 1px solid red;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+
+.link {
+  text-decoration: none;
+  color: inherit;
+}
+
+.capital {
+  font-size: 24px;
+}
+
+.invest {
+  margin-right: 10px;
+  font-size: 24px;
+  color: #f1c40f;
+  font-weight: bold;
+}
+
+.left {
+  display: flex;
+  align-items: center;
+}
+
+.right {
+  display: flex;
+  align-items: center;
+}
+</style>
