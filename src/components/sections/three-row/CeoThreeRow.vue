7c617694b028a88c82cdad39d37ba83325f56a82
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-20 02:40:37 -0500

    added ceo three row

---
+++ src/components/sections/three-row/CeoThreeRow.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import './ceo-three-row.scss'
+</script>
+
+<template>
+  <section class="ceo-three-row-container">
+    <div class="ceo-three-row-left">
+      
+      <p>
+        Meticulously designed to offer everything for a comfortable long or short term stay, from essential modern
+        amenities to unexpected luxuries like a pool, private gym, or a breathtaking rooftop terrace.
+      </p>
+
+      <div class="ceo-info">
+        <div>
+          <img class="ceo-image" src="/images/widgets/widget-ceo1.webp" alt="alt" width="78" height="78" />
+        </div>
+        <div>
+          <p class="ceo-name">Matthias Nadler</p>
+          <p>Ceo</p>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <span class="first-letter">L</span>
+      <p>Luxurious short-term and monthly stays, personalized service, exclusive homes, ideal for extended visits,
+        renovations, or work relocations. </p>
+    </div>
+
+    <div>
+      <p>Tailored, safe, and fully furnished luxury vacation rentals with exceptional amenities, reflecting the vision
+        of our CEO for unparalleled hospitality.</p>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import BillboardComponent from '@/components/billboard/BillboardComponent.vue'
 import ImageTitle from '@/components/sections/two-row/image-title/ImageTitle.vue'
+import CeoThreeRow from '@/components/sections/three-row/CeoThreeRow.vue'
 </script>
 
 
 <template>
   <BillboardComponent />
   <ImageTitle />
+  <CeoThreeRow />
 </template>
