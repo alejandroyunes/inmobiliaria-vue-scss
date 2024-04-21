5ffaaadd19c3f84b8e941e62bd2a998d4e3b36db
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-21 12:44:35 -0500

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import Header from './components/header/HeaderComponent.vue';
+import Header from './components/header/HeaderComponent.vue'
+import Footer from './components/footer/FooterComponent.vue'
 </script>
 
 <template>
   <Header />
-  
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped></style>
