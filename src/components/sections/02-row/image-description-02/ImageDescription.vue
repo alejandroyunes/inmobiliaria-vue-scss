ba4d67ab4b877e8dea77c92401d4debdc8d15664
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-21 21:45:07 -0500

    adding services

---
+++ src/components/sections/02-row/image-description-02/ImageDescription.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import './image-description.scss'
+</script>
+
+
+<template>
+  <section>
+    <div class="container">
+      <div class="main">
+        <img class="image" src="/images/sections/main-three-images/service1.webp" alt="alt" width={0} height={0} />
+      </div>
+      <div>
+        <h3 class="blogSlogan">Real Estate</h3>
+        <p class="description">Experience top-notch real estate solutions through our tailored services. We showcase
+          your property to a global audience, attracting quality travelers for short-term stays or long-term leases.
+          From seamless booking to checkout, we ensure a hassle-free experience. Choose flexible options for rentals
+          that suit your needs. Our personalized approach customizes services to your property and goals.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="mainMiddle">
+        <img class="image" src="/images/sections/main-three-images/service1.webp" alt="alt" width={0} height={0} />
+      </div>
+      <div>
+        <h3 class="blogSlogan">Management</h3>
+        <p class="description">Entrust your vacation home to our expert care and enjoy the benefits while we handle all
+          property needs. As Your Trusted Partner in Real Estate, Property Management we ensure your property is always
+          guest-ready. Our dedicated team manages cleaning, maintenance, and repairs with a second-to-none approach,
+          turning your property into a welcoming haven.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="main">
+        <img class="image" src="/images/sections/main-three-images/service1.webp" alt="alt" width={0} height={0} />
+      </div>
+      <div>
+        <h3 class="blogSlogan">Construction</h3>
+        <p class="description">Receive expert construction support from planning to completion. Our commitment to
+          quality craftsmanship ensures your project meets high standards, delivered on time and within budget.
+          Leveraging local expertise, we provide informed recommendations and navigate challenges. For dreamers of
+          construction projects, our third branch offers steadfast support, turning visions into reality with
+          comprehensive assistance. Trust us for a seamless, successful construction journey.</p>
+      </div>
+    </div>
+  </section>
+</template>
