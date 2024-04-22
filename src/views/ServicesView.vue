f0adba56cfceaa44142ec85175e136c7cea60ba3
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-21 23:20:10 -0500

    added services page

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import TitleArrow from '@/components/sections/01-row/title-arrow/TitleArrow.vue'
-import ImageDescription from '@/components/sections/02-row/image-description-02/ImageDescription.vue'
+import ImageDescription from '@/components/sections/02-row/image-description-services/ImageDescriptionServices.vue'
 </script>
 
 <template>
