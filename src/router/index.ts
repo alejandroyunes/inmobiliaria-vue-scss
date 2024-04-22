fe04930d76c3c8087e0689fbe6c7e896b7c7c59f
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-22 00:15:07 -0500

    adding routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,16 @@ const router = createRouter({
       name: 'contact',
       component: () => import('../views/ContactView.vue')
     }
-  ]
+  ],
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth'
+      }
+    }
+    return { top: 0 }
+  }
 })
 
 export default router
